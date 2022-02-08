35c31cc62e4833d8e809f5707b108b8ded9328c2
Aaronlamz <qylinxia@qq.com>
2022-02-08 17:16:57 +0800

    init types

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { App, ref, reactive, readonly, toRaw } from 'vue'
+import {
+  I18nOptions,
+  I18nInstance
+} from './types'
+
+export function createI18n(options:I18nOptions):I18nInstance{
+
+}
---
+++ src/types.ts
@@ -0,0 +1,22 @@
+import { App, Ref } from 'vue'
+
+export type I18nOptions = {
+  locale?: string
+  messages?: {
+    [locale: string]: LocaleMessages
+  }
+}
+
+export type I18nInstance = {
+   install(app: App): void
+}
+
+export type LocaleMessages = {
+  [key: string]: LocaleMessageObject | LocaleMessage
+}
+
+export type LocaleMessageObject = {
+  [key: string]: LocaleMessageObject | LocaleMessage
+}
+
+export type LocaleMessage = string
