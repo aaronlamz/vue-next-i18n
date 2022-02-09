265e4ca98919408b452e6f4d0704ceae4c4d429f
Aaronlamz <qylinxia@qq.com>
2022-02-09 19:59:15 +0800

    add examples

---
+++ examples/vite-project/src/App.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <h2>Current Lang :</h2>
+  <h2>Test Cases:</h2>
+  <p>Key[button.add]: {{ $t('button.add') }}</p>
+  <p>Key[function.tips]: {{ $t('function.tips', 'val') }}</p>
+  <button>Change Lang</button>
+</template>
+<script  lang="ts">
+export default {
+  i18n: {
+    en: {
+      button: {
+        add: 'i18nOption Add new',
+        'function.tips': (val: string) => `new ${val}`
+      }
+    },
+    zhCHS: {
+      button: {
+        add: 'i18n选项 新增',
+        'function.tips': (val: string) => `new ${val}`
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ examples/vite-project/src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createI18n } from '../../../src'
+
+const i18n = createI18n({
+    locale: 'en',
+    messages: {
+        en: {
+            button: {
+                add: 'Add new'
+            }
+        },
+        zhCHS: {
+            button: {
+                add: '新增'
+            }
+        }
+    }
+})
+createApp(App).use(i18n).mount('#app')
