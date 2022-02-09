8f09582b085d051aaca69832b38d54bc1320d087
Aaronlamz <qylinxia@qq.com>
2022-02-09 11:32:56 +0800

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,30 @@ module.exports = {
     root: true,
     env: {
         node: true,
-        jest: true,
+        jest: true
     },
     extends: [
-        "plugin:vue/vue3-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:prettier/recommended",
-        "prettier",
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:prettier/recommended',
+        'prettier'
     ],
-    plugins: ["@typescript-eslint"],
-    parser: "vue-eslint-parser",
+    plugins: ['@typescript-eslint'],
+    parser: 'vue-eslint-parser',
     parserOptions: {
-        parser: "@typescript-eslint/parser",
-        sourceType: "module",
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
     },
     rules: {
-        "object-curly-spacing": "off",
-        "@typescript-eslint/ban-types": "off",
-        "@typescript-eslint/ban-ts-ignore": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/member-delimiter-style": "off",
-        "@typescript-eslint/no-use-before-define": "off",
-        "@typescript-eslint/no-var-requires": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-    },
-};
+        'object-curly-spacing': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/member-delimiter-style': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off'
+    }
+}
