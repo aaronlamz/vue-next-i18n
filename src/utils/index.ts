c77ff9aa89402d1c596f090400143159a5baaa51
Aaronlamz <qylinxia@qq.com>
2022-07-29 18:42:16 +0800

    test: update coverage --test

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,24 +9,6 @@ export function isDef(value: any) {
     return value !== undefined && value !== null
 }
 
-export function parsePath(path: string): Array<string> {
-    const pathArray = path.split('.')
-    const parts = []
-
-    for (let i = 0; i < pathArray.length; i++) {
-        let p = pathArray[i]
-
-        while (p[p.length - 1] === '\\' && pathArray[i + 1] !== undefined) {
-            p = p.slice(0, -1) + '.'
-            p += pathArray[++i]
-        }
-
-        parts.push(p)
-    }
-
-    return parts
-}
-
 export function getMessage(
     messageObject: LocaleMessageObject,
     path: string
